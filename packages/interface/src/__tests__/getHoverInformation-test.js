1e055a323bcf957209a0c73b0bf840ffe49019ee
Sol Plant <sol@twitter.com>
2018-02-16 15:38:44 +0000

    npm run pretty

--- packages/interface/src/__tests__/getHoverInformation-test.js
+++ packages/interface/src/__tests__/getHoverInformation-test.js
@@ -29,40 +29,63 @@ describe('getHoverInformation', () => {
     schema = buildSchema(schemaIDL);
   });
 
-  function testHover(
-    query: string,
-    point: Position,
-  ): Hover.contents {
+  function testHover(query: string, point: Position): Hover.contents {
     return getHoverInformation(schema, query, point);
   }
 
   it('provides leaf field information', () => {
-    const actual = testHover('query { thing { testField } }', new Position(0, 20));
-    expect(actual).to.deep.equal('TestType.testField: String\n\n This is field documentation for TestType.testField');
+    const actual = testHover(
+      'query { thing { testField } }',
+      new Position(0, 20),
+    );
+    expect(actual).to.deep.equal(
+      'TestType.testField: String\n\n This is field documentation for TestType.testField',
+    );
   });
 
   it('provides aliased field information', () => {
-    const actual = testHover('query { thing { other: testField } }', new Position(0, 25));
-    expect(actual).to.deep.equal('TestType.testField: String\n\n This is field documentation for TestType.testField');
+    const actual = testHover(
+      'query { thing { other: testField } }',
+      new Position(0, 25),
+    );
+    expect(actual).to.deep.equal(
+      'TestType.testField: String\n\n This is field documentation for TestType.testField',
+    );
   });
 
   it('provides intermediate field information', () => {
-    const actual = testHover('query { thing { testField } }', new Position(0, 10));
-    expect(actual).to.deep.equal('Query.thing: TestType\n\n This is field documentation for Query.thing');
+    const actual = testHover(
+      'query { thing { testField } }',
+      new Position(0, 10),
+    );
+    expect(actual).to.deep.equal(
+      'Query.thing: TestType\n\n This is field documentation for Query.thing',
+    );
   });
 
   it('provides list field information', () => {
-    const actual = testHover('query { listOfThing { testField } }', new Position(0, 10));
+    const actual = testHover(
+      'query { listOfThing { testField } }',
+      new Position(0, 10),
+    );
     expect(actual).to.deep.equal('Query.listOfThing: [TestType!]');
   });
 
   it('provides deprecated field information', () => {
-    const actual = testHover('query { thing { testDeprecatedField } }', new Position(0, 20));
-    expect(actual).to.deep.equal('TestType.testDeprecatedField: Float\n\nDeprecated: deprecation reason');
+    const actual = testHover(
+      'query { thing { testDeprecatedField } }',
+      new Position(0, 20),
+    );
+    expect(actual).to.deep.equal(
+      'TestType.testDeprecatedField: Float\n\nDeprecated: deprecation reason',
+    );
   });
 
   it('provides enum field information', () => {
-    const actual = testHover('query { thing { testEnumField } }', new Position(0, 20));
+    const actual = testHover(
+      'query { thing { testEnumField } }',
+      new Position(0, 20),
+    );
     expect(actual).to.deep.equal('TestType.testEnumField: Color');
   });
 
@@ -72,18 +95,25 @@ describe('getHoverInformation', () => {
   });
 
   it('provides parameter type information', () => {
-    const actual = testHover('query { parameterizedField(id: "foo") { testField } }', new Position(0, 28));
+    const actual = testHover(
+      'query { parameterizedField(id: "foo") { testField } }',
+      new Position(0, 28),
+    );
     expect(actual).to.deep.equal('Query.parameterizedField(id: String!)');
   });
 
   it('provides directive information', () => {
-    const actual = testHover('query { thing { testField @skip(if:true) } }', new Position(0, 30));
-    expect(actual).to.deep.equal('@skip\n\nDirects the executor to skip this field or fragment when the `if` argument is true.');
+    const actual = testHover(
+      'query { thing { testField @skip(if:true) } }',
+      new Position(0, 30),
+    );
+    expect(actual).to.deep.equal(
+      '@skip\n\nDirects the executor to skip this field or fragment when the `if` argument is true.',
+    );
   });
 
   it('provides union information', () => {
     const actual = testHover('query { unionField }', new Position(0, 12));
     expect(actual).to.deep.equal('Query.unionField: UnionType');
   });
-
 });
